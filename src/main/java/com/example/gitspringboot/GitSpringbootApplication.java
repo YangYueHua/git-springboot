f9f8107b5fb5927e6b2088adb41d21615c329620
yuehua <yangyuehua008@qq.com>
2022-06-24 10:03:00 +0800

    3 commit

--- src/main/java/com/example/gitspringboot/GitSpringbootApplication.java
+++ src/main/java/com/example/gitspringboot/GitSpringbootApplication.java
@@ -9,6 +9,8 @@ public class GitSpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitSpringbootApplication.class, args);
         System.out.println("abc");
+        System.out.println("22222222222");
+        System.out.println("333333333333");
     }
 
 }
